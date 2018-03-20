ed386ae87e7cf7fe29cdabfe2438fa3c403862ee
ryanbomo <ryanbomo@gmail.com>
2018-03-19 22:15:36 -0400

    worked on it

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-
+export BROWSER=chromium-browser
 cd ../src
-python3 -i main.py
+python3.5 main.py
--- src/main.py
+++ src/main.py
@@ -3,14 +3,76 @@
 #Date: 03/19/2018
 
 from board import *
+from vpython import *
+import copy
+
+class Ball:
+    def __init__(self, vect, rad, col,dest_coordinates):
+        self.vector = vect
+        #self.arrow_vect = copy.copy(vect)
+        self.color = col
+        self.radius = rad
+        self.destination = dest_coordinates
+        self.vpython_object = sphere(pos=self.vector, radius=self.radius, color=self.color)
+       # self.arrow = arrow(pos=self.vpython_object.pos, axis=self.arrow_vect, color=color.yellow)
         
-def main():
-    board = Board(10,10)
-    coords = [[[1,1],[3,5]],
-              [[2,2],[5,8]]]
-    for i in coords:
-        board.createPoint(i[0],i[1])
-    for i in board.points:
-        print(i.getLocation())
+    def move(self,d_X, d_Y):
+        # Update X and Y
+        self.vector.x += d_X
+        self.vector.y += d_Y
         
-main()
+        # Update Arrow X and Y
+        #self.arrow_vect.x = 10*d_X
+        #self.arrow_vect.y = 10*d_Y
+        
+        # Update vpython Objects
+        self.vpython_object.pos = self.vector
+        #self.arrow.pos = self.vector
+        #self.arrow.axis = self.arrow_vect
+        
+    def step_to_destination(self):
+        print("Fart")
+        
+        
+        
+red_balls_info = [[vector(9,1,0),[-10,1]],
+             [vector(9,0,0),[-10,0]],
+             [vector(9,-1,0),[-10,-1]],
+             [vector(10,1,0),[-9,1]],
+             [vector(10,0,0),[-9,0]],
+             [vector(10,-1,0),[-9,-1]]]
+green_balls_info = [[vector(1,10,0),[1,-9]],
+               [vector(0,10,0),[0,-9]],
+               [vector(-1,10,0),[-1,-9]],
+               [vector(1,9,0),[1,-10]],
+               [vector(0,9,0),[0,-10]],
+               [vector(-1,9,0),[-1,-10]]]
+cyan_balls_info = [[vector(1,-10,0),[1,9]],
+              [vector(0,-10,0),[0,9]],
+              [vector(-1,-10,0),[-1,9]],
+              [vector(1,-9,0),[1,10]],
+              [vector(0,-9,0),[0,10]],
+              [vector(-1,-9,0),[-1,10]]]
+blue_balls_info = [[vector(-9,1,0), [10,1]],
+              [vector(-9,0,0), [10,0]],
+              [vector(-9,-1,0), [10,-1]],
+              [vector(-10,1,0),[9,1]],
+              [vector(-10,0,0),[9,0]],
+              [vector(-10,-1,0),[9,-1]]]
+
+blue_balls = []
+red_balls = []
+green_balls = []
+cyan_balls = []
+
+for i in blue_balls_info:
+    blue_balls.append(Ball(i[0],0.1,color.blue,i[1]))
+    
+for i in green_balls_info:
+    green_balls.append(Ball(i[0],0.1,color.green,i[1]))
+
+for i in red_balls_info:
+    red_balls.append(Ball(i[0],0.1,color.red,i[1]))
+
+for i in cyan_balls_info:
+    cyan_balls.append(Ball(i[0],0.1,color.cyan,i[1]))
